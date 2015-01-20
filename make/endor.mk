9c3784631be0f3602f19bac0d0327945f2fa8bcd
Alan Potter <alan.potter@calnexsol.com>
2015-01-20 11:05:25 +0000

    Fixing endor so as not to build CAT

--- make/endor.mk
+++ make/endor.mk
@@ -210,9 +210,11 @@ endor-unpack: $(ENDOR_BUILD_DIR)/.configured
 $(ENDOR_BUILD_DIR)/.built: $(ENDOR_BUILD_DIR)/.configured 
 	rm -f $@
 	$(MAKE) -C $(@D)
-	echo "Thinking about making the CAT"
-	pwd
-	$(MAKE) cat
+	#echo "Thinking about making the CAT"
+	#pwd
+	#$(MAKE) cat
+    echo Copying the previously-built CAT
+    echo Endor build is $(ENDOR_BUILD_DIR)
 	touch $@
 
 #
