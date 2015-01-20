3e1734419ff0a01ed7c845eeaa508e075361c239
Alan Potter <alan.potter@calnexsol.com>
2015-01-20 11:07:09 +0000

    Typo

--- make/endor.mk
+++ make/endor.mk
@@ -213,8 +213,8 @@ $(ENDOR_BUILD_DIR)/.built: $(ENDOR_BUILD_DIR)/.configured
 	#echo "Thinking about making the CAT"
 	#pwd
 	#$(MAKE) cat
-    echo Copying the previously-built CAT
-    echo Endor build is $(ENDOR_BUILD_DIR)
+    echo "Copying the previously-built CAT"
+    echo "Endor build is $(ENDOR_BUILD_DIR)"
 	touch $@
 
 #
