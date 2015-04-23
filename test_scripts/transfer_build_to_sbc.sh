feab1b8e6b50dccf6df3470f04037406c48f3dc5
Alan Potter <alan.potter@calnexsol.com>
2015-04-23 12:06:19 +0100

    Add SSH options to make it not boke when connecting to VMs

--- test_scripts/transfer_build_to_sbc.sh
+++ test_scripts/transfer_build_to_sbc.sh
@@ -10,6 +10,8 @@ fi
 TARGETHOST=$1
 REFERENCEHOST=$2
 
+SSH_OPTIONS=-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+
 # Make sure the host is there, bomb out if it's not
 #
 ping -c 1 ${TARGETHOST}
@@ -22,16 +24,16 @@ fi
 # Relies on SSH keys being correctly set up
 #
 echo "Stopping services on $1"
-ssh ${TARGETHOST} "/opt/etc/init.d/S99endor-webapp stop"
-ssh ${TARGETHOST} "/opt/etc/init.d/S98cat-remotingserver stop"
-ssh ${TARGETHOST} "/opt/etc/init.d/S97endor-instrumentcontroller stop"
-ssh ${TARGETHOST} "/opt/etc/init.d/S96endor-virtualinstrument stop"
-ssh ${TARGETHOST} "/opt/etc/init.d/S95postgresql stop"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "/opt/etc/init.d/S99endor-webapp stop"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "/opt/etc/init.d/S98cat-remotingserver stop"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "/opt/etc/init.d/S97endor-instrumentcontroller stop"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "/opt/etc/init.d/S96endor-virtualinstrument stop"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "/opt/etc/init.d/S95postgresql stop"
 
 # Delete the entire old application folder
 #
 echo "Removing old build on $1"
-ssh ${TARGETHOST} "~/test_helpers/remove_old_endor_folder.sh"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "~/test_helpers/remove_old_endor_folder.sh"
 
 # And copy the new folder to the target machine
 #
@@ -41,17 +43,17 @@ if [ -d "/tmp/endor_staging" ]; then
 fi
 
 echo "Copying files from ${REFERENCE_HOST} to localhost"
-scp -r ${REFERENCEHOST}:/opt/lib/endor /tmp/endor_staging
+scp ${SSH_OPTIONS}  -r ${REFERENCEHOST}:/opt/lib/endor /tmp/endor_staging
 
 echo "Copying files to ${TARGETHOST} from localhost"
-scp -r /tmp/endor_staging ${TARGETHOST}:/opt/lib/endor
+scp ${SSH_OPTIONS} -r /tmp/endor_staging ${TARGETHOST}:/opt/lib/endor
 
 # Restart operations on test machine
 echo "Restarting services"
-ssh ${TARGETHOST} "/opt/etc/init.d/S95postgresql start"
-ssh ${TARGETHOST} "cd /opt/lib/endor/schema/Baseline; python3 RebuildDb.py --superuser=calnex"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "/opt/etc/init.d/S95postgresql start"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "cd /opt/lib/endor/schema/Baseline; python3 RebuildDb.py --superuser=calnex"
 
-ssh ${TARGETHOST} "voodoo reboot"
+ssh ${TARGETHOST} ${SSH_OPTIONS} "voodoo reboot"
 
 echo "Waiting for ${TARGETHOST} to reboot"
 sleep 45
