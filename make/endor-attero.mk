74ada9a4682c411f03df0618aad8323bee0e04fb
Alan Potter <alan.potter@calnexsol.com>
2015-06-01 16:19:39 +0100

    Typo in path name

--- make/endor-attero.mk
+++ make/endor-attero.mk
@@ -249,8 +249,8 @@ $(ENDOR_ATTERO_IPK): $(ENDOR_ATTERO_BUILD_DIR)/.built-attero
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/get_subnet_mask.sh     $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/get_subnet_mask.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/poweroff.sh            $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/poweroff.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/reboot.sh              $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/reboot.sh
-	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Web/WebApp/Shell/Shell/set_time.sh                                         $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/set_time.sh
-	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Web/WebApp/Shell/Shell/set_date.sh                                         $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/set_date.sh
+	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Web/WebApp/Shell/set_time.sh                                               $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/set_time.sh
+	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Web/WebApp/Shell/set_date.sh                                               $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/set_date.sh
 	
 	# CAT HTML and Javascript
 	#
