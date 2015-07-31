0ad27f7780501ef92471a2e6c44ef7b1d46f6f13
Dougal Stanton <dougal.stanton@calnexsol.com>
2015-07-31 16:27:51 +0100

    Wait for embedded before starting IC and Web App
    * Abort IC startup after 2min if embedded doesn't appear.
    * Aborted IC triggers DB failure message and DHCP.
    * Delay Web app startup for 2m30s for IC to appear.
    * Web app timeout triggers further DB failure message.
    * IP fallback increased from 30s to 300s only as last resort.

--- make/endor-attero.mk
+++ make/endor-attero.mk
@@ -286,6 +286,7 @@ $(ENDOR_ATTERO_IPK): $(ENDOR_ATTERO_BUILD_DIR)/.built-attero
 	install -d $(ENDOR_ATTERO_IPK_DIR)/opt/etc/init.d
 	install -m 755 $(ENDOR_ATTERO_SOURCE_DIR)/instrumentcontroller-supervisor   $(ENDOR_ATTERO_IPK_DIR)/opt/bin/instrumentcontroller-supervisor
 	install -m 755 $(ENDOR_ATTERO_SOURCE_DIR)/cat-supervisor                    $(ENDOR_ATTERO_IPK_DIR)/opt/bin/cat-supervisor
+	install -m 755 $(ENDOR_ATTERO_SOURCE_DIR)/calnex.endor.webapp               $(ENDOR_ATTERO_IPK_DIR)/opt/bin/calnex.endor.webapp
 	install -m 755 $(ENDOR_ATTERO_SOURCE_DIR)/calnex.endor.translatorclui       $(ENDOR_ATTERO_IPK_DIR)/opt/bin/calnex.endor.translatorclui 
 	install -m 755 $(ENDOR_ATTERO_SOURCE_DIR)/curiosity                         $(ENDOR_ATTERO_IPK_DIR)/opt/bin/curiosity
 	install -m 755 $(ENDOR_ATTERO_SOURCE_DIR)/cat-redirect                      $(ENDOR_ATTERO_IPK_DIR)/opt/bin/cat-redirect
