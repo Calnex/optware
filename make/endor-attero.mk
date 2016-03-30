3ab29e5888db1b1a467054214b953ad0eca62718
DeanLynch <dean.lynch@calnexsol.com>
2016-03-30 11:01:00 +0100

    Added get_endor_revision.sh to the Attero makefile

--- make/endor-attero.mk
+++ make/endor-attero.mk
@@ -327,6 +327,7 @@ $(ENDOR_ATTERO_IPK): $(ENDOR_ATTERO_BUILD_DIR)/.built
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/get_gateway.sh         $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/get_gateway.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/get_ip.sh              $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/get_ip.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/get_subnet_mask.sh     $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/get_subnet_mask.sh
+	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/get_endor_revision.sh  $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/get_endor_revision.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/poweroff.sh            $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/poweroff.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Shell/reboot.sh              $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/reboot.sh
 	install -m 755 $(ENDOR_ATTERO_BUILD_DIR)/Endor/Instrument/Calnex.Endor.Instrument.Controller/Sql/attero_updates.sql       $(ENDOR_ATTERO_IPK_DIR)/opt/lib/endor/attero_updates.sql
