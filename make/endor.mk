eade36f7fdad11723c51275a9ef736d2aad4ad33
DeanLynch <dean.lynch@calnexsol.com>
2016-07-19 10:06:33 +0100

    Re added tshark

--- make/endor.mk
+++ make/endor.mk
@@ -42,7 +42,7 @@ ENDOR_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 ENDOR_DESCRIPTION=Describe endor-$(ENDOR_PRODUCT) here.
 ENDOR_SECTION=base
 ENDOR_PRIORITY=optional
-ENDOR_DEPENDS=postgresql, mono, xsp, nginx, endor-$(ENDOR_PRODUCT)-doc
+ENDOR_DEPENDS=postgresql, mono, xsp, nginx, tshark, endor-$(ENDOR_PRODUCT)-doc
 ENDOR_SUGGESTS=
 ENDOR_CONFLICTS=endor-paragon, endor-paragon-doc
 ifeq "${ENDOR_PRODUCT}" "paragon"
