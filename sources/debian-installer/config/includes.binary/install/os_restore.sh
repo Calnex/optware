939c468f01ab2f9b2f08c646d8034a88dc2642e7
jenkins <jenkins@calnexsol.com>
2017-02-07 11:31:28 +0000

    Moved the preseed parts of debconf to the script os_restore.sh, added the sleeps to fix a bug with installing from F7. Added Script os_upgrade.sh for moving legacy builds to the new partitioning setup

---
+++ sources/debian-installer/config/includes.binary/install/os_restore.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+set -e
+
+# unpack OS files
+mkdir /os-install
+cd /os-install
+tar zxvf /cdrom/optware/debian*
+tar zxvf data.tar.gz
+cd opt/var/lib/debian
+
+# write bootloader
+dd if=boot.img of=/dev/sda
+sleep 0.2
+sync
+sleep 0.2
+
+# write root partition
+blockdev --rereadpt /dev/sda
+sleep 1
+dd if=root.img of=/dev/sda1 bs=8M
+sync
+
+reboot
