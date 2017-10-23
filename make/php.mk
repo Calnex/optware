b82e4f326d870112cb4b74760d694d7b370ef3aa
DeanLynch <dean.lynch@calnexsol.com>
2017-10-23 15:36:00 +0100

    Unmarked the .ini file as userconf to stop prompt

--- make/php.mk
+++ make/php.mk
@@ -31,7 +31,7 @@ PHP_IPK_VERSION=3
 #
 # PHP_CONFFILES should be a list of user-editable files
 #
-PHP_CONFFILES=/opt/etc/php.ini
+#PHP_CONFFILES=/opt/etc/php.ini
 
 #
 # PHP_LOCALES defines which locales get installed
