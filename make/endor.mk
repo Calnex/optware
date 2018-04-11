89d1f4c87ab784872d1977858b2f18a13bafe30b
Will McFarlane <will.mcfarlane@calnexsol.com>
2018-04-11 15:26:41 +0100

    Revert previous commit. No effect. Fix erroneous CONFLICT variable.

--- make/endor.mk
+++ make/endor.mk
@@ -48,7 +48,7 @@ ENDOR_CONFLICTS=endor-paragon, endor-paragon-doc, endor-paragon-neo, endor-parag
 ifeq "${ENDOR_PRODUCT}" "paragon"
 	ENDOR_CONFLICTS=endor-attero, endor-attero-doc, endor-paragon-neo, endor-paragon-neo-doc
 else ifeq "${ENDOR_PRODUCT}" "paragon-neo"
-	ENDOR_CONFLICTS=endor-attero, endor-attero-doc, endor-paragon, endor-paragon-neo
+	ENDOR_CONFLICTS=endor-attero, endor-attero-doc, endor-paragon, endor-paragon-doc
 endif
 
 #
@@ -275,7 +275,8 @@ $(ENDOR_IPK): $(ENDOR_BUILD_DIR)/.built
 	install -m 755 $(ENDOR_SOURCE_DIR)/postrm   $(ENDOR_IPK_DIR)/CONTROL/postrm
 	echo $(ENDOR_CONFFILES) | sed -e 's/ /\n/g' > $(ENDOR_IPK_DIR)/CONTROL/conffiles
 	
-	$(ENDOR_BUILD_DIR)/OptWare/Make/endor-makefile install $(BUILD_DIR) $(ENDOR_PRODUCT); \
+	$(ENDOR_BUILD_DIR)/OptWare/Make/endor-makefile install $(BUILD_DIR) $(ENDOR_PRODUCT);
+
 	# Embedded firmware
 	#
 	if [ ! -z "${ENDOR_FIRMWARE_VERSION}" ]; then \
