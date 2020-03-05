9173e0b2f131fb3ae3453c6a50ea98d66e08e998
Jenkins <jenkins@calnexsol.com>
2020-03-05 14:36:27 +0000

    incremented php so it's ahead of wheezy

--- make/php.mk
+++ make/php.mk
@@ -27,7 +27,7 @@ PHP_CONFLICTS=debian (<= 9.0)
 #
 # PHP_IPK_VERSION should be incremented when the ipk changes.
 #
-PHP_IPK_VERSION=7
+PHP_IPK_VERSION=8
 
 #
 # PHP_CONFFILES should be a list of user-editable files
