7518f22522b8034c5a8356d8b87fe220ef478a9b
Barry Somerville <barry.somerville@calnexsol.com>
2021-02-15 17:33:37 +0000

    For EFI, write the bootable image directly to sda - no parted in installer OS to make partitions

--- sources/debian-installer/config/includes.binary/install/os_restore.sh
+++ sources/debian-installer/config/includes.binary/install/os_restore.sh
@@ -13,38 +13,11 @@ cp /cdrom/test/root.iso .
 
 if [ -e /sys/firmware/efi ]
 then
-  echo "Configuring EFI system"
+  echo "Configuring EFI system..."
   
-  if ! blkid | grep sda;
-  then
-    echo "No disk label"
-    echo "Making gpt disk label..."
-    parted /dev/sda mklabel gpt
-  fi
-  
-  # Delete existing boot/rootfs partitions, ignore "partition doesn't exist" errors
-  set +e
-  parted /dev/sda rm 1 2>/dev/null
-  parted /dev/sda rm 2 2>/dev/null
-  parted /dev/sda rm 3 2>/dev/null
-  set -e
-  
-  echo "Creating partition 1 - EFI - 512Mb"
-  parted /dev/sda mkpart EFI 2048s 1050623s
-  
-  echo "Creating partition 2 - rootfs(1) - 2Gb"
-  parted /dev/sda mkpart rootfs1 1050624s 5244927s
-  
-  echo "Creating partition 2 - rootfs(2) - 2Gb"
-  parted /dev/sda mkpart rootfs2 5244928s 9439231s
-  
-  echo "Writing EFI boot partition..."
-  dd if=boot.iso of=/dev/sda1
-  
-  echo "Writing rootfs partition..."
-  dd if=root.iso of=/dev/sda2 bs=8M
+  dd if=bootable.iso of=/dev/sda bs=8M
 else
-  echo "Configuring BIOS system"
+  echo "Configuring BIOS system..."
   
   # write bootloader
   dd if=boot.img of=/dev/sda
