1149de4d95fa4f25d299de698301f514357819e6
Jamie Barnes <jamie.barnes@calnexsol.com>
2021-02-16 11:04:31 +0000

    Changed package name in control file (debian-efi)

--- make/debian-efi.mk
+++ make/debian-efi.mk
@@ -213,7 +213,7 @@ debian-efi-stage: $(DEBIAN-EFI_BUILD_DIR)/.staged
 $(DEBIAN-EFI_IPK_DIR)/CONTROL/control:
 	@install -d $(@D)
 	@rm -f $@
-	@echo "Package: debian" >>$@
+	@echo "Package: debian-efi" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
 	@echo "Priority: $(DEBIAN-EFI_PRIORITY)" >>$@
 	@echo "Section: $(DEBIAN-EFI_SECTION)" >>$@
