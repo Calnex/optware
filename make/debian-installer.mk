c2b109b6f829aff05f2e73f936c8bfd329b39695
Barry Somerville <barry.somerville@calnexsol.com>
2021-02-19 08:55:53 +0000

    Build installer as hdd/ext4

--- make/debian-installer.mk
+++ make/debian-installer.mk
@@ -127,7 +127,8 @@ $(DEBIAN-INSTALLER_BUILD_DIR)/.configured: $(DEBIAN-INSTALLER_PATCHES) make/debi
 	# # /usr/lib/live/build/config --help							\
 	sudo lb config										\
 		--architectures				amd64					\
-		--binary-images				iso-hybrid				\
+		--binary-images				hdd				\
+		--binary-filesystem			ext4				\
 		--distribution				stretch					\
 		--memtest				memtest86+				\
 		--checksums				sha1					\
