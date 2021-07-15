a2d4dc3591e5a62f40bc362245e10af0aa571839
Amelie Levray <amelie.l41.30@gmail.com>
2021-07-15 15:54:22 +0100

    bye ssh back to token

--- make/endor.mk
+++ make/endor.mk
@@ -33,7 +33,7 @@ BUILD_DATE?=dd-mm-yyyy
 ENDOR_BRANCH_PARAM?=master
 
 
-ENDOR_REPOSITORY?=git@github.com:Calnex/Springbank.git
+ENDOR_REPOSITORY?=https://github.com/Calnex/Springbank
 ENDOR_PRODUCT=$(TARGET_PRODUCT_LOWER)
 ENDOR_DOCUMENTATION_REPOSITORY?=https://github.com/Calnex/EndorDocumentation
 ENDOR_VERSION=$(shell echo "$(BUILD_VERSION_NUMBER)" | cut --delimiter "." --output-delimiter "." -f2,3,4)
