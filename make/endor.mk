dbcfc6a87d6a4e66664781d9f1b968c27d03f149
David Kolosowski <david.kolosowski@calnexsol.com>
2021-07-26 16:48:21 +0100

    Change build tool to dotnet rather than msbuild.

--- make/endor.mk
+++ make/endor.mk
@@ -216,8 +216,8 @@ $(ENDOR_BUILD_DIR)/.built: $(ENDOR_BUILD_DIR)/.configured
 	rm -f $@
 	#ABSOLUTE_PATH=$(@D);
 	(cd $(@D);\
-		msbuild -t:restore -p:RestorePackagesPath=Libs/CAT/Calnex.Endor.DataStorage/Calnex.Common/Libraries; \
-		$(BUILD_TOOL) Endor.sln /p:CustomConstants="$(ENDOR_BUILD_CONSTANTS)" /p:Configuration=Release $(TOOL_PATH))
+		dotnet restore
+		dotnet build --configuration Release /p:CustomConstants="$(ENDOR_BUILD_CONSTANTS)"
 	touch $@
 
 
