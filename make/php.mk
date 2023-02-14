25580a383bb9529e9deb1b4def0a7121189d9598
StathisIoannidis <106957227+StathisIoannidis@users.noreply.github.com>
2023-02-14 15:54:48 +0000

    PN-3926

--- make/php.mk
+++ make/php.mk
@@ -254,7 +254,7 @@ $(PHP_BUILD_DIR)/.configured: $(DL_DIR)/$(PHP_SOURCE) $(PHP_PATCHES) make/php.mk
 		--with-pcre-regex=$(STAGING_PREFIX) \
 		$(PHP_CONFIGURE_ARGS) \
 		--without-pear \
-		--with-curl=/usr \
+		--with-curl=shared,$(STAGING_PREFIX) \
 	)
 	$(PATCH_LIBTOOL) $(@D)/libtool
 	touch $@
