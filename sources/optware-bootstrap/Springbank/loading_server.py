bf3be47102e45b71f043f76049794d3291b6edb5
StathisIoannidis <106957227+StathisIoannidis@users.noreply.github.com>
2023-02-28 09:47:42 +0000

    PN-3978 Trying to fix the python script loading and serving the Http server

--- sources/optware-bootstrap/Springbank/loading_server.py
+++ sources/optware-bootstrap/Springbank/loading_server.py
@@ -1,17 +1,20 @@
-from os import curdir, sep
-from os.path import join as pjoin
-
+import ssl
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class StoreHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         with open('/var/www/loading.html') as fh:
-                                                self.send_response(200)
-                                                self.send_header('Content-type', 'text/html')
-                                                self.end_headers()
-                                                self.wfile.write(fh.read().encode())
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(fh.read().encode())
 
+# Create an HTTP server on port 8080
+http_server = HTTPServer(('', 8080), StoreHandler)
+http_server.serve_forever()
 
-server = HTTPServer(('', 8080), StoreHandler)
-server.serve_forever()
+# Create an HTTPS server on port 8443
+https_server = HTTPServer(('', 8443), StoreHandler)
+https_server.socket = ssl.wrap_socket(https_server.socket, certfile='/home/calnex/Certificates/hold.pem', keyfile='/home/calnex/Certificates/hold.pem', server_side=True)
+https_server.serve_forever()
