d9f42568ced16f4e9cbd02aa68a92de9a373a3e9
Tauqir Pervez <tauqir.pervez@calnexsol.com>
2023-07-13 15:45:38 +0100

    Try copying the missing file?

--- make/postgresql.mk
+++ make/postgresql.mk
@@ -150,16 +150,7 @@ postgresql-unpack: $(POSTGRESQL_BUILD_DIR)/.configured
 $(POSTGRESQL_BUILD_DIR)/.built: $(POSTGRESQL_BUILD_DIR)/.configured
 	rm -f $@
 
-	echo $${MAKELEVEL}
-	echo $${MAKEFLAGS}
-	echo $${MFLAGS}
-	echo "About to unset make flags"
-	#unset MAKELEVEL && unset MAKEFLAGS && unset MFLAGS
-	export MAKELEVEL=
-
-	echo $${MAKELEVEL}
-	echo $${MAKEFLAGS}
-	echo $${MFLAGS}
+	cp /home/jenkins/workspace/ManagementTools/Optware_Package/builds/postgresql/src/backend/utils/errcodes.h /home/jenkins/workspace/ManagementTools/Optware_Package/builds/postgresql/src/include/utils/
 
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(POSTGRESQL_CPPFLAGS)" \
