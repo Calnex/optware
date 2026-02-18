4b3b1ff59cd4aa7b64d8c64e4a89ec2eb223b2da
Jamie Barnes <jamie.barnes@calnexsol.com>
2026-02-18 14:29:07 +0000

    Only rename the ipkg-opt directory if required

--- make/ipkg-opt.mk
+++ make/ipkg-opt.mk
@@ -94,7 +94,9 @@ ipkg-opt-source: $(DL_DIR)/$(IPKG-OPT_SOURCE)
 $(IPKG-OPT_BUILD_DIR)/.configured: $(DL_DIR)/$(IPKG-OPT_SOURCE)
 	rm -rf $(BUILD_DIR)/$(IPKG-OPT_DIR) $(@D)
 	tar -C $(BUILD_DIR) -xzf $(DL_DIR)/$(IPKG-OPT_SOURCE)
-	mv $(BUILD_DIR)/ipkg-opt-0.99.163 $(BUILD_DIR)/$(IPKG-OPT_DIR) # Rename extracted directory
+	if test -d $(BUILD_DIR)/ipkg-opt-$(IPKG-OPT_VERSION) ; \
+		mv $(BUILD_DIR)/ipkg-opt-$(IPKG-OPT_VERSION) $(BUILD_DIR)/$(IPKG-OPT_DIR) ; \
+	fi
 	if test -n "$(IPKG-OPT_PATCHES)" ; \
 		then cat $(IPKG-OPT_PATCHES) | \
 		patch -d $(BUILD_DIR)/$(IPKG-OPT_DIR) -p1 ; \
