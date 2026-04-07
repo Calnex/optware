fc33307e81ec9f7f8c1f6e9f6ecc1e09ed29c310
Jamie Barnes <jamie.barnes@calnexsol.com>
2026-04-07 10:19:10 +0100

    Removing file that was not intended for commit

--- push_usb_changes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REMOTE_HOST=${1:-192.168.205.41}
-LOCAL_PATH=/home/dev/build/optware/sources/optware-bootstrap/usbrepo
-REMOTE_PATH=usbdev
-
-# Copy files to remote host
-scp -r "$LOCAL_PATH"/* "calnex@$REMOTE_HOST:$REMOTE_PATH"
